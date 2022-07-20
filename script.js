e118796aa6fda8b333f8144638c1d66fb766f7fd
KamajiRD <adamescarlos2808@gmail.com>
2022-07-20 00:36:45 -0400

    Added Loop for rounds

--- script.js
+++ script.js
@@ -12,31 +12,55 @@ function getComputerChoice() {
     return gameAnswers[Math.floor(Math.random()* gameAnswers.length)]
 };
 
+// Scores
+let playerScore = 0;
+let computerScore = 0;
+
+// Function to get player's choice
 function getPlayerChoice() {
     return prompt("What's your choice?")
 };
 
-// Variables
-const computerSelection = getComputerChoice();
-let playerSelection = getPlayerChoice()
+// Selections
+// const computerSelection = getComputerChoice();
+// const playerSelection = getPlayerChoice();
 
-let playerScore = 0;
-let computerScore = 0;
+
 
 function playRound(playerSelection, computerSelection){
+
     if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper"){
+        computerScore++
         return "You Lost! Paper beats Rock"
     } else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors") {
+        playerScore++
         return "You Win! Rock beats Scissors"
     } else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "rock") {
         return "It's a Tie!"
     } 
+
 };
 
+//Loop function to play 5 rounds and print the result of each
 function game() {
-    for (let i = 0; i < 5; i++) {
-       
-    } 
+    for(let i = 0; i < 5; i++){
+        let playerSelection = prompt("Pick a move");
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("your score = " + playerScore);
+        console.log("Computer's score = " + computerScore);
+    }
 }
 
+
 game();
+
+
+/* 
+
+When a user inputs an option compare with computers selection
+If the user wins, give 1 point to the user
+If the computer wins, give 1 point to the computer
+Loop the round 5 times and print the result of each round
+
+*/
